b7b37df29a479e8535a4321d3e7c7c9243ee4cba
Payton Yao <payton.yao@gmail.com>
2018-02-23 07:20:31 +0800

    Expand included items instead of just throwing them away

--- rest_framework_json_api/parsers.py
+++ rest_framework_json_api/parsers.py
@@ -30,6 +30,61 @@ class JSONParser(parsers.JSONParser):
     renderer_class = renderers.JSONRenderer
 
     @staticmethod
+    def create_naive_included_map(data):
+        included = data.get('included')
+        included_map = dict()
+        if not included:
+            included = dict()
+        for obj in included:
+            _type = obj['type']
+            _id = obj['id']
+            included_map[_type, _id] = obj
+        return included_map
+
+    @staticmethod
+    def get_included_object(included_map, relation):
+        # Check to see if the relation points to an included object
+        # If it does not, just return the original relation
+        if 'type' in relation and 'id' in relation:
+            _type = relation['type']
+            _id = relation['id']
+            return included_map.get((_type, _id), relation)
+        else:
+            return relation
+
+    @staticmethod
+    def create_included_map(data):
+        naive_map = JSONParser.create_naive_included_map(data)
+        # For each item in the 'included' section, we want to update all the objects
+        # under their relationships to be the other included items and not just references
+        for (_type, _id), obj in naive_map.iteritems():
+            relationships = obj.get('relationships')
+            if not relationships:
+                relationships = dict()
+
+            for field_name, field_data in relationships.items():
+                inner_data = field_data.get('data')
+                if isinstance(inner_data, dict):
+                    # If we find a direct link, we replace it
+                    field_data['data'] = JSONParser.get_included_object(naive_map, inner_data)
+                elif isinstance(inner_data, list):
+                    # Otherwise, we replace the list of links
+                    field_data['data'] = map(
+                        lambda obj: JSONParser.get_included_object(naive_map, obj),
+                        inner_data
+                    )
+
+        included_map = dict()
+        for (_type, _id), obj in naive_map.iteritems():
+            result = {'id': obj.get('id')} if 'id' in obj else {}
+            result['type'] = obj.get('type')
+            result.update(JSONParser.parse_attributes(obj))
+            if 'relationships' in obj:
+                result['relationships'] = obj['relationships']
+            included_map[_type, _id] = result
+        return included_map
+
+    @staticmethod
     def parse_attributes(data):
         attributes = data.get('attributes')
         uses_format_translation = getattr(settings, 'JSON_API_FORMAT_KEYS', False)
@@ -43,7 +98,7 @@ class JSONParser(parsers.JSONParser):
             return attributes
 
     @staticmethod
-    def parse_relationships(data):
+    def parse_relationships(data, included_map):
         uses_format_translation = getattr(settings, 'JSON_API_FORMAT_KEYS', False)
         relationships = data.get('relationships')
 
@@ -53,14 +108,20 @@ class JSONParser(parsers.JSONParser):
             # convert back to python/rest_framework's preferred underscore format
             relationships = utils.format_keys(relationships, 'underscore')
 
+        def canonical(relation):
+            return JSONParser.get_included_object(included_map, relation)
+
         # Parse the relationships
         parsed_relationships = dict()
         for field_name, field_data in relationships.items():
             field_data = field_data.get('data')
-            if isinstance(field_data, dict) or field_data is None:
-                parsed_relationships[field_name] = field_data
+            if isinstance(field_data, dict):
+                parsed_relationships[field_name] = canonical(field_data)
             elif isinstance(field_data, list):
-                parsed_relationships[field_name] = list(relation for relation in field_data)
+                parsed_relationships[field_name] = \
+                    list(canonical(relation) for relation in field_data)
+            elif field_data is None:
+                parsed_relationships[field_name] = field_data
         return parsed_relationships
 
     @staticmethod
@@ -104,6 +165,7 @@ class JSONParser(parsers.JSONParser):
             return data
 
         request = parser_context.get('request')
+        included_map = self.create_included_map(result)
 
         if isinstance(data, list):
             # This section is a shim to allow JSON API to PUT/POST/PATCH multiple objects at once
@@ -133,7 +195,7 @@ class JSONParser(parsers.JSONParser):
                 parsed_object = {'id': obj.get('id')} if 'id' in obj else {}
                 parsed_object['type'] = obj.get('type')
                 parsed_object.update(self.parse_attributes(obj))
-                parsed_object.update(self.parse_relationships(obj))
+                parsed_object.update(self.parse_relationships(obj, included_map))
                 parsed_object.update(self.parse_metadata(result))
                 parsed_data.append(parsed_object)
             return parsed_data
@@ -162,6 +224,6 @@ class JSONParser(parsers.JSONParser):
             parsed_data = {'id': data.get('id')} if 'id' in data else {}
             parsed_data['type'] = data.get('type')
             parsed_data.update(self.parse_attributes(data))
-            parsed_data.update(self.parse_relationships(data))
+            parsed_data.update(self.parse_relationships(data, included_map))
             parsed_data.update(self.parse_metadata(result))
             return parsed_data
